a0306ca6d99ce911b0d5656f882ff4538dbe1d45
DarioPenagos <dpenagosv@unal.edu.co>
2023-03-04 06:44:13 -0500

    Intenté eliminar todos los errores sin prestar atención a la funcionalidad del programa

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,16 +10,18 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
+        //this(0);
     }
 
     public Asignatura(int codigoInterno,Tipo tipo) {
         this("Sin nombre", codigoInterno, 0,tipo);
     }
 
+    /* 
     public Asignatura(int codigoExterno,Tipo tipo) {
         this("Sin nombre", 0, codigoExterno,tipo);
     }
+    */
 
     public Asignatura(String nombre, Tipo tipo) {
         this(nombre, 0, 0,tipo);
@@ -43,9 +45,11 @@ public class Asignatura {
         this.codigoInterno = codigoInterno;
     }
 
+    /*
     public void cambiarDatos(int codigoExterno) {
         this.codigoExterno = codigoExterno;
     }
+    */
 
     public void cambiarDatos(String nombre) {
         this.nombre = nombre;
